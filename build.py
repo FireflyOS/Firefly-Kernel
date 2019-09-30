9654fd1ed44b29d9becb2170c920bfc641f50662
Yuhanun <yuhanun.c@gmail.com>
2019-09-30 22:30:57 +0200

    Push sorry

--- build.py
+++ build.py
@@ -1,8 +1,14 @@
+import time
 import platform
 
 import colorama
 
 import building.booter
+
+if platform.system() != "Linux":
+    colorama.init(convert=True)
+
+begin = time.time()
 
 if platform.system() != "Linux":
     colorama.init(convert=True)
@@ -15,3 +21,5 @@ for name, build_task in build_dict.items():
     if build_task():
         continue
     print(f"{colorama.Fore.RED}Failed to build FireflyOS on step {name}{colorama.Fore.RESET}")
+
+print(f"{colorama.Fore.GREEN}Build finished in {round(time.time() - begin, 2)} seconds{colorama.Fore.RESET}")
