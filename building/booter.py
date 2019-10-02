52c31b9d1d09c77416cd302f4293ba8703aacd0c
Yuhanun Citgez <yuhanun.c@gmail.com>
2019-10-02 18:31:13 +0200

    Updated boot.asm, stuck in infinite loop

--- building/booter.py
+++ building/booter.py
@@ -4,17 +4,19 @@ import platform
 import colorama
 
 
-nasm_path = "C:\\Users\\John\\AppData\\Local\\bin\\NASM\\nasm.exe"
+nasm_path = "nasm"
 
 commands = [
     f"{nasm_path} -f elf64 ./booter/boot.asm",
     f"{nasm_path} -f elf64 ./booter/multiboot_header.asm",
-    "ld -n -o ./binaries/bootloader/kernel.bin -T ./booter/linker.ld ./booter/multiboot_header.o ./booter/boot.o"
+    "ld -n -o ./binaries/boot/kernel.bin -T ./booter/linker.ld ./booter/multiboot_header.o ./booter/boot.o",
+    "grub-mkrescue -o FireflyOS.iso binaries"
 ]
 
 post_build = [
     (os.remove, ["./booter/multiboot_header.o"], ),
     (os.remove, ["./booter/boot.o"], ),
+    (shutil.move, ["FireflyOS.iso", "binaries/boot/FireflyOS.iso"])
 ]
 
 
@@ -22,8 +24,9 @@ def build() -> bool:
     """
     Builds the bootloader
     """
-    if not os.path.isdir("./binaries/bootloader"):
-        os.makedirs("./binaries/bootloader")
+    if not os.path.isdir("./binaries/boot"):
+        os.makedirs("./binaries/boot")
+
     for command in commands:
         if os.system(command):
             print(f"{colorama.Fore.RED}Building the bootloader failed at command:\n{colorama.Fore.GREEN}{command}{colorama.Fore.RESET}")
