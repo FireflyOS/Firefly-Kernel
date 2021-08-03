41ae4eeecde887b9adad78480f146c62e894802b
V01D-NULL <timster23@protonmail.com>
2021-08-03 20:22:24 +0200

    Add maybe_unused tag

--- include/utils.hpp
+++ include/utils.hpp
@@ -3,7 +3,7 @@
 
 // If you ever need to just build the kernel to see if there are any errors
 // without getting pedantic errors on unused variables, use this.
-#define IGNORE_PEDANTIC_ERROR(var) ({ decltype(var) var = var; })
+#define IGNORE_PEDANTIC_ERROR(var) ({ [[maybe_unused]] decltype(var) var = var; })
 
 void start_load(cursor& crs, const char* _str);
 void end_load(cursor& crs, const char* _str);
