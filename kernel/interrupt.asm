68b439342798fab9679677772950bcd61984cc5f
V01D-NULL <timster23@protonmail.com>
2021-08-03 13:34:42 +0200

    Fix assembly interrupt routine

--- kernel/interrupt.asm
+++ kernel/interrupt.asm
@@ -6,25 +6,7 @@ global exception_wrapper
 extern interrupt_handler
 extern exception_handler
 
-interrupt_wrapper:
-    push rax
-    push rbx
-    push rcx
-    push rdx
-    push rsi
-    push rdi
-    push r8
-    push r9
-    push r10
-    push r11
-    push r12
-    push r13
-    push r14
-    push r15
-    ; also save SSE state when we figure that out
-
-    call interrupt_handler
-
+%macro popa64 0
     pop r15
     pop r14
     pop r13
@@ -39,9 +21,9 @@ interrupt_wrapper:
     pop rcx
     pop rbx
     pop rax
-    iret
+%endmacro
 
-exception_wrapper:
+%macro pusha64 0
     push rax
     push rbx
     push rcx
@@ -56,24 +38,26 @@ exception_wrapper:
     push r13
     push r14
     push r15
+%endmacro
+
+interrupt_wrapper:
+    cld
+    pusha64
+    ; also save SSE state when we figure that out
+
+    call interrupt_handler
+
+    popa64
+    iretq
+
+exception_wrapper:
+    cld
+    pusha64
     ; also save SSE state when we figure that out
 
     ; pass error code
     push qword [rsp + (13 * 8)]
     call exception_handler
 
-    pop r15
-    pop r14
-    pop r13
-    pop r12
-    pop r11
-    pop r10
-    pop r9
-    pop r8
-    pop rdi
-    pop rsi
-    pop rdx
-    pop rcx
-    pop rbx
-    pop rax
-    iret
+    popa64
+    iretq
