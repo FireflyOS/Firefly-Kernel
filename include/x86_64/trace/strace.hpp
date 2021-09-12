6a57053c79f1301c32c8227df22bef6e3b56179a
SergeyMC9730 <bdsservers@yandex.ru>
2021-09-12 10:05:31 +0800

    Error Codes

--- include/x86_64/trace/strace.hpp
+++ include/x86_64/trace/strace.hpp
@@ -1,9 +1,20 @@
 #pragma once
 
 namespace firefly::trace {
-constexpr const char* P_OUTOFMEMORY     = "Out of Memory Error";
-constexpr const char* P_DRIVERERROR     = "Driver Error";
-constexpr const char* P_MANUALLYCRASHED = "Manually Crashed";
+constexpr const char* PM_OUTOFMEMORY     = "OUT_OF_MEMORY_EXCEPTION";
+constexpr const char* PM_DRIVERERROR_PK  = "PS2_KEYBOARD_DRIVER_ERROR";
+constexpr const char* PM_DRIVERERROR_S   = "SERIAL_PORT_DRIVER_ERROR";
+constexpr const char* PM_DRIVERERROR_V   = "VBE_DRIVER_ERROR";
+constexpr const char* PM_MANUALLYCRASHED = "MANUALLY_CRASHED";
+
+constexpr const int   PC_OUTOFMEMORY     = 0x0E000F;
+constexpr const int   PC_DRIVERERROR_PK  = 0x0F3C40;
+constexpr const int   PC_DRIVERERROR_S   = 0x0F1450;
+constexpr const int   PC_DRIVERERROR_V   = 0x0FFF00;
+constexpr const int   PC_MANUALLYCRASHED = 0xFFFFFF;
+
+//PM — Panic Message
+//PC — Panic Code
 void trace_stack();
 void panic(const char *msg);
 }  // namespace firefly::trace
