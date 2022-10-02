136a8d4f4e51303c61686b3ca43eb1e942249a65
Vincent Schweiger <vincent.schweiger@xolley.de>
2022-10-02 18:40:04 +0200

    Forgot to merge the 8GB

--- scripts/qemu-uefi.sh
+++ scripts/qemu-uefi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-qemu-system-x86_64 -smp 4 -enable-kvm -M smm=off -cpu host -m 1G -boot d -no-shutdown -serial stdio -no-reboot -bios /usr/share/ovmf/OVMF.fd -cdrom ../FireflyOS_x86_64.iso
+qemu-system-x86_64 -smp 4 -enable-kvm -M smm=off -cpu host -m 8G -boot d -no-shutdown -serial stdio -no-reboot -bios /usr/share/ovmf/OVMF.fd -cdrom ../FireflyOS_x86_64.iso
