7948530356e989426251be0acd018ba01e1fd505
V01D-NULL <timster23@protonmail.com>
2023-03-12 14:02:27 -0500

    CPU IDs are not guaranteed to be in sequantial order, so we read the id from the apic instead

--- firefly/kernel/intel64/cpu/cpu.cpp
+++ firefly/kernel/intel64/cpu/cpu.cpp
@@ -38,15 +38,6 @@ void initializeThisCpu(uint64_t stack) {
     auto cpuData = new (mm::heap->allocate(sizeof(CpuData))) CpuData;
     cpuData->selfPointer = cpuData;
 
-    allCpuContextsLock.lock();
-    cpuData->cpuIndex = allCpuContexts.size();
-    allCpuContextsLock.unlock();
-
-    allCpuContexts.push(cpuData);
-
-    logLine << "initializing CPU " << fmt::dec << cpuData->cpuIndex << '\n'
-            << fmt::endl;
-
     core::gdt::init(cpuData->gdt);
     core::tss::init(cpuData, stack);
 
@@ -54,8 +45,13 @@ void initializeThisCpu(uint64_t stack) {
     wrmsr(MSR::GsBase, reinterpret_cast<uint64_t>(cpuData));
 
     firefly::kernel::core::interrupt::init();
-
     apic::Apic::init();
+
+    cpuData->cpuIndex = apic::Apic::accessor().apicId();
+    allCpuContexts.push(cpuData);
+
+    logLine << "Initialized CPU " << fmt::dec << cpuData->cpuIndex << '\n'
+            << fmt::endl;
 }
 
 }  // namespace firefly::kernel
